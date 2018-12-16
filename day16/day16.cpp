ccaa61b83dd1864a970d765ce585268dd65a6ddb
CheezeCake <emmanuel.nicolet@gmail.com>
2018-12-16 12:15:02 +0100

    day16: cleanup

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -9,8 +9,6 @@
 
 class VM
 {
-	std::array<int, 4> regs = {0, 0, 0, 0};
-
 public:
 	struct Instr
 	{
@@ -20,11 +18,27 @@ public:
 		int C;
 	};
 
-	int fetchRegister(int r)
+	using RegisterSet = std::array<int, 4>;
+
+	VM()
+	{
+	}
+
+	VM(const RegisterSet& rs)
+	{
+		setRegisters(rs);
+	}
+
+	int getRegister(int r) const
 	{
 		if (r < 0 || r >= regs.size())
-			throw std::runtime_error("fetching register: " + std::to_string(r));
+			throw std::runtime_error("getting register: " + std::to_string(r));
 		return regs[r];
+	}
+
+	RegisterSet getRegisters() const
+	{
+		return regs;
 	}
 
 	void setRegister(int r, int v)
@@ -34,149 +48,131 @@ public:
 		regs[r] = v;
 	}
 
+	void setRegisters(const RegisterSet& rs)
+	{
+		std::copy(std::begin(rs), std::end(rs), std::begin(regs));
+	}
+
 	void exec(const Instr& i)
 	{
 		switch (i.opcode) {
 			case 0:
-				setRegister(i.C, fetchRegister(i.A) + fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) + getRegister(i.B));
 				break;
 			case 1:
-				setRegister(i.C, fetchRegister(i.A) + i.B);
+				setRegister(i.C, getRegister(i.A) + i.B);
 				break;
 			case 2:
-				setRegister(i.C, fetchRegister(i.A) * fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) * getRegister(i.B));
 				break;
 			case 3:
-				setRegister(i.C, fetchRegister(i.A) * i.B);
+				setRegister(i.C, getRegister(i.A) * i.B);
 				break;
 			case 4:
-				setRegister(i.C, fetchRegister(i.A) & fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) & getRegister(i.B));
 				break;
 			case 5:
-				setRegister(i.C, fetchRegister(i.A) & i.B);
+				setRegister(i.C, getRegister(i.A) & i.B);
 				break;
 			case 6:
-				setRegister(i.C, fetchRegister(i.A) | fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) | getRegister(i.B));
 				break;
 			case 7:
-				setRegister(i.C, fetchRegister(i.A) | i.B);
+				setRegister(i.C, getRegister(i.A) | i.B);
 				break;
 			case 8:
-				setRegister(i.C, fetchRegister(i.A));
+				setRegister(i.C, getRegister(i.A));
 				break;
 			case 9:
 				setRegister(i.C, i.A);
 				break;
 			case 10:
-				setRegister(i.C, i.A > fetchRegister(i.B));
+				setRegister(i.C, i.A > getRegister(i.B));
 				break;
 			case 11:
-				setRegister(i.C, fetchRegister(i.A) > i.B);
+				setRegister(i.C, getRegister(i.A) > i.B);
 				break;
 			case 12:
-				setRegister(i.C, fetchRegister(i.A) > fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) > getRegister(i.B));
 				break;
 			case 13:
-				setRegister(i.C, i.A == fetchRegister(i.B));
+				setRegister(i.C, i.A == getRegister(i.B));
 				break;
 			case 14:
-				setRegister(i.C, fetchRegister(i.A) == i.B);
+				setRegister(i.C, getRegister(i.A) == i.B);
 				break;
 			case 15:
-				setRegister(i.C, fetchRegister(i.A) == fetchRegister(i.B));
+				setRegister(i.C, getRegister(i.A) == getRegister(i.B));
 				break;
 			default:
 				throw std::runtime_error("invalid opcode: " + std::to_string(i.opcode));
 		}
 	}
+
+private:
+	RegisterSet regs = {0, 0, 0, 0};
 };
 
 int main()
 {
+	std::array<std::set<int>, 16> opcodeMatches;
+	for (auto& matches : opcodeMatches)
+		matches = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
 	int matchThree = 0;
 
-	std::array<std::array<bool, 16>, 16> opcodeMatches;
-	for (auto& matches : opcodeMatches)
-		matches.fill(true);
-
 	while (true) {
-		int before[4];
+		VM::RegisterSet before;
 		if (scanf("Before: [%d, %d, %d, %d]\n", &before[0], &before[1], &before[2], &before[3]) != 4)
 			break;
 
 		struct VM::Instr instr;
 		int iOpcode;
-		if (scanf("%d %d %d %d\n", &iOpcode, &instr.A, &instr.B, &instr.C) != 4)
-			break;
+		scanf("%d %d %d %d\n", &iOpcode, &instr.A, &instr.B, &instr.C);
 
-		int after[4];
-		if (scanf("After: [%d, %d, %d, %d]\n", &after[0], &after[1], &after[2], &after[3]) != 4)
-			break;
+		VM::RegisterSet after;
+		scanf("After: [%d, %d, %d, %d]\n", &after[0], &after[1], &after[2], &after[3]);
 
 		int matchOpcodes = 0;
 		for (int mOpcode = 0; mOpcode <= 15; ++mOpcode) {
-			VM vm;
-			for (int i = 0; i < 4; ++i)
-				vm.setRegister(i, before[i]);
+			VM vm{before};
 
 			instr.opcode = mOpcode;
 			vm.exec(instr);
-			bool match{true};
-			for (int i = 0; match && i < 4; ++i) {
-				if (vm.fetchRegister(i) != after[i])
-					match = false;
-			}
-			if (match) {
+			if (vm.getRegisters() == after)
 				++matchOpcodes;
-				/* opcodeMatches[iOpcode].[mOpcode] = true; */
-			}
-			else {
-				opcodeMatches[iOpcode][mOpcode] = false;
-			}
+			else
+				opcodeMatches[iOpcode].erase(mOpcode);
 		}
 
-		std::cout << "match : " << matchOpcodes << '\n';
 		if (matchOpcodes >= 3)
 			++matchThree;
 	}
 
 	std::cout << "part 1: " << matchThree << '\n';
 
-	std::vector<std::pair<int, int>> known;
+	bool done;
 	do {
-		for (int j = 0; j < opcodeMatches.size(); ++j) {
-			for (auto k : known) {
-				if (j != k.first)
-					opcodeMatches[j][k.second] = false;
-			}
-		}
-		known.clear();
-		for (int j = 0; j < opcodeMatches.size(); ++j) {
-			int n = 0;
-			int match;
-			for (int i = 0; i < opcodeMatches[j].size(); ++i) {
-				if (opcodeMatches[j][i]) {
-					++n;
-					match = i;
+		done = true;
+		for (int i{0}; i < opcodeMatches.size(); ++i) {
+			if (opcodeMatches[i].size() > 1) continue;
+			int m{*std::begin(opcodeMatches[i])};
+			for (int j{0}; j < opcodeMatches.size(); ++j) {
+				if (j == i) continue;
+				if (opcodeMatches[j].find(m) != std::end(opcodeMatches[j])) {
+					opcodeMatches[j].erase(m);
+					done = false;
 				}
 			}
-			std::cout << n << '\n';
-			if (n == 1) known.push_back({j, match});
 		}
-		std::cout << '\n';
-	} while (known.size() < 16);
+	} while (!done);
 
 	std::array<int, 16> iToM;
 	for (int i = 0; i < opcodeMatches.size(); ++i) {
-		int m;
-		for (int j = 0; j < opcodeMatches[i].size(); ++j)
-			if (opcodeMatches[i][j])
-				m = j;
-		iToM[i] = m;
+		iToM[i] = *std::begin(opcodeMatches[i]);
 	}
 
 	VM vm;
-	// read prog
 	while (true) {
 		struct VM::Instr instr;
 		int n = scanf("%d %d %d %d\n", &instr.opcode, &instr.A, &instr.B, &instr.C);
@@ -187,5 +183,5 @@ int main()
 		vm.exec(instr);
 	}
 
-	std::cout << "part 2: " << vm.fetchRegister(0) << '\n';;
+	std::cout << "part 2: " << vm.getRegister(0) << '\n';;
 }
